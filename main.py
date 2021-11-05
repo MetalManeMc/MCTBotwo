2e7f815a0dbbc1385e3d2fc70ed0f795321f3a38
MetalManiac&Smajlo <93709189+MetalManiacAndSmajlo@users.noreply.github.com>
2021-11-05 14:07:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import discord
-from discord_slash import SlashCommand
+from discord_slash import SlashCommand, create_option
+
+
+@slash.slash(name="translate",
+             description="Translates a Minecraft string from a language to another.",
+             options=[
+                 create_option(
+                     name="sourcelang",
+                     description="Source language code. Will be the translation key if not defined",
+                     option_type=3,
+                     required=False),
+                 create_option(
+                     name="string",
+                     description="The string to translate",
+                     option_type=3,
+                     required=True),
+                 create_option(
+                     name="target",
+                     description="Source language code. Will be the translation key if not defined",
+                     option_type=3,
+                     required=False)])
+async def translate(ctx, sourcelang="key"):
+    await ctx.send(content=sourcelang)
