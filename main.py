60c83eadd04570fbad4fa4417051fe0db882e479
MetalManiac&Smajlo <93709189+MetalManiacAndSmajlo@users.noreply.github.com>
2021-11-05 14:32:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ slash = SlashCommand(client, sync_commands=True)
              description="Translates a Minecraft string from a language to another.",
              options=[
                  create_option(
-                     name="sourcelang",
-                     description="Source language code. Will be the translation key if not defined",
-                     option_type=3,
-                     required=False),
-                 create_option(
                      name="string",
                      description="The string to translate",
                      option_type=3,
                      required=True),
                  create_option(
+                     name="sourcelang",
+                     description="Source language code. Will be the translation key if not defined",
+                     option_type=3,
+                     required=False),
+                 create_option(
                      name="target",
                      description="Source language code. Will be the translation key if not defined",
                      option_type=3,
                      required=False)])
-async def translate(ctx, sourcelang="key"):
-    await ctx.send(content=sourcelang)
+async def translate(ctx, string,  sourcelang="key", target="key"):
+    await ctx.send(content=string + sourcelang + target)
 
 client.run(open("token.txt").read)
