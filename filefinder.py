7532aad276a77e9a736da1248a179e746b9fb715
MetalManiacMc <metalmaniac1331@outlook.com>
2021-11-07 18:43:20 +0100

    Now extracts the language files into a seperate folder

--- filefinder.py
+++ filefinder.py
@@ -11,7 +11,11 @@ for lang in langs:                          # For all element in the index
 
 for dirpath, dirnames, filenames in os.walk("objects"): # Runs through the minecraft assets
     for file in filenames:                              # For each file
-        if file not in codes and ".json" not in file:                           # If the name of the files aren't in the language hash list
+        if file not in codes and ".json" not in file:   # If the name of the files aren't in the language hash list
             os.remove(os.path.join(dirpath, file))      # Deletes them
         elif "json" not in file:                        # Else
             os.rename(os.path.join(dirpath, file), os.path.join(dirpath, file) + ".json")   # Renames them in ".json"
+
+for dirpath, dirnames, filenames in os.walk("objects"):
+    for file in filenames:
+        os.replace(os.path.join(dirpath, file), os.path.join("lang", file))
