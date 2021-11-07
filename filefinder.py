fd59fe6678e4b5cfa0d31d68a3d280e1d4fae422
MetalManiacMc <metalmaniac1331@outlook.com>
2021-11-07 09:17:21 +0100

    Added descriptions

--- filefinder.py
+++ filefinder.py
@@ -3,15 +3,15 @@
 import os
 import json
 
-langs = json.load(open("langcodes.json"))
+langs = json.load(open("langcodes.json"))   # Opens the language index (from the minecraft assets)
 codes = []
 
-for lang in langs:
-    codes.append(langs[lang]["hash"])
+for lang in langs:                          # For all element in the index
+    codes.append(langs[lang]["hash"])       # Notes the "hash", which is the code of used for the game to know which assets are which
 
-for dirpath, dirnames, filenames in os.walk("objects"):
-    for file in filenames:
-        if file not in codes:
-            os.remove(os.path.join(dirpath, file))
-        else:
-            os.rename(os.path.join(dirpath, file), os.path.join(dirpath, file) + ".json")
+for dirpath, dirnames, filenames in os.walk("objects"): # Runs through the minecraft assets
+    for file in filenames:                              # For each file
+        if file not in codes:                           # If the name of the files aren't in the language hash list
+            os.remove(os.path.join(dirpath, file))      # Deletes them
+        else:                                           # Else
+            os.rename(os.path.join(dirpath, file), os.path.join(dirpath, file) + ".json")   # Renames them in ".json"
