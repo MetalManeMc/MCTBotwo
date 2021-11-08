cc4780510a14139bf44b1d91482673dd027a5e5b
MetalManiacMc <metalmaniac1331@outlook.com>
2021-11-08 19:40:01 +0100

    Lang files are now renamed

--- filefinder.py
+++ filefinder.py
@@ -7,7 +7,12 @@ langs = json.load(open("langcodes.json"))   # Opens the language index (from the
 codes = []
 
 def rename(initname):
-    pass
+    for x in langs:
+        if langs[x]["hash"]+".json" == initname:
+            tempname=x
+    newname=tempname.replace("minecraft/lang/", "")
+    return newname
+
 
 for lang in langs:                          # For all element in the index
     codes.append(langs[lang]["hash"])       # Notes the "hash", which is the code of used for the game to know which assets are which
@@ -15,12 +20,17 @@ for lang in langs:                          # For all element in the index
 for dirpath, dirnames, filenames in os.walk("objects"): # Runs through the minecraft assets
     for file in filenames:                              # For each file
         if file not in codes and ".json" not in file:   # If the name of the files aren't in the language hash list
-#            os.remove(os.path.join(dirpath, file))      # Deletes them
-            pass
+            os.remove(os.path.join(dirpath, file))      # Deletes them
         elif "json" not in file:                        # Else
-            langfile=rename(file)
-            os.rename(os.path.join(dirpath, langfile), os.path.join(dirpath, langfile) + ".json")   # Renames them in ".json"
+            newfile=rename(file)                        # Renames the files variable...
+            os.rename(os.path.join(dirpath, file), os.path.join(dirpath, rename(file)))         # ... before renaming the actual file
+            os.rename(os.path.join(dirpath, newfile), os.path.join(dirpath, newfile) + ".json") # Renames them in ".json"
 
 for dirpath, dirnames, filenames in os.walk("objects"):
     for file in filenames:
-        os.replace(os.path.join(dirpath, file), os.path.join("lang", file))
+        os.replace(os.path.join(dirpath, file), os.path.join("lang", file)) # Places the lang files in the "lang" folder. The "objects" one should be empty
+
+for dirpath, dirnames, filenames in os.walk("lang"):
+    for file in filenames:
+        if "-" not in file:                                                             # If not already done...
+            os.rename(os.path.join(dirpath, file), os.path.join(dirpath, rename(file))) # ... renames the file
