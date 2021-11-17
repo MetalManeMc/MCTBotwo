514bc638db1f6d9a1f1a69497075e5b2e0c0b7d8
MetalManiacMc <93709189+MetalManiacMc@users.noreply.github.com>
2021-11-17 17:19:52 +0100

    Formatting

--- main.py
+++ main.py
@@ -3,45 +3,46 @@ import discord
 from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_option
 
-client = discord.Client(intents = discord.Intents.all())
+client = discord.Client(intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True)
 
 guild_ids = [906169345007304724]
 
+
 def translater(string, source, target):
-    source=json.load(open("lang/"+source+".json"))
-    target=json.load(open("lang/"+target+".json"))
+    source = json.load(open("lang/"+source+".json"))
+    target = json.load(open("lang/"+target+".json"))
     for key in source:
-        if source[key]==string:
+        if source[key] == string:
             return target[key]
     return "Invalid string"
-    
 
-@slash.slash(name = "translate",
-             description = "Translates a Minecraft string from a language to another.",
-             guild_ids = guild_ids,
-             options = [
+
+@slash.slash(name="translate",
+             description="Translates a Minecraft string from a language to another.",
+             guild_ids=guild_ids,
+             options=[
                  create_option(
-                     name = "string",
-                     description = "The string or key to translate.",
-                     option_type = 3,
-                     required = True
+                     name="string",
+                     description="The string or key to translate.",
+                     option_type=3,
+                     required=True
                  ),
                  create_option(
-                     name = "target",
-                     description = "Code of the language, in which the string is going to be sent.",
-                     option_type = 3,
-                     required = True
+                     name="target",
+                     description="Code of the language, in which the string is going to be sent.",
+                     option_type=3,
+                     required=True
                  ),
                  create_option(
-                     name = "sourcelang",
-                     description = "Code of the language, in which you wrote the string. (English is default)",
-                     option_type = 3,
-                     required = False
+                     name="sourcelang",
+                     description="Code of the language, in which you wrote the string. (English is default)",
+                     option_type=3,
+                     required=False
                  )
              ]
-            )
-async def translate(ctx, string, target,  sourcelang = "en_us"):
+             )
+async def translate(ctx, string, target,  sourcelang="en_us"):
     await ctx.send(translater(string, sourcelang, target))
 
 client.run(open("token.txt").read())
