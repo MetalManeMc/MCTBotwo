a5d754031856732234e881d23d51ef279d3048b6
SmajloSlovakian <16209307+SmajloSlovakian@users.noreply.github.com>
2021-12-01 11:03:22 +0100

    langname, code and google return update
    language finding now works without the files being renamed... if they are renamed, you're just essentially disabling one way of finding the language and making the code less efficient... made up two new variables: langcodesapp, langname and the order of searching is: source(codesapp, codes, name), target(codesapp, codes, name)
    added one parameter to find(): outputindex = False
    made google return a tuple of (message, info type, info)
    message is the output we used until now, so every call of google should be indexed with [0]
    info type is something like metadata, it is a number and says what type of info is being returned
    info is the translation or list of them or an error code... what it actually is is said in the [1] index

--- main.py
+++ main.py
@@ -14,22 +14,38 @@ debug=False # Mark as True if you are on your pc and don't want to turn the bot 
 guild_ids = [906169345007304724]
 
 
-def find(search, inside, outputlist = False, isdictionary = False, errorOut = True): # Function that returns a list or first find of found searches inside a list or dictionary
+def find(search, inside, outputlist = False, isdictionary = False, errorOut = True, outputindex = False): # Function that returns a list or first find of found searches inside a list or dictionary
     o = []
     if not isdictionary: # Searches for keys or if it is list, the values of the list
-        for i in inside:
-            if search.lower() in i.lower():
-                if outputlist:
-                    o.append(i)
-                else:
-                    return i
+        if outputindex:
+            for i in range(len(inside)-1):
+                if search.lower() in inside[i].lower():
+                    if outputlist:
+                        o.append(i)
+                    else:
+                        return i
+        else:
+            for i in inside:
+                if search.lower() in i.lower():
+                    if outputlist:
+                        o.append(i)
+                    else:
+                        return i
     else: # Searches for values instead of keys
-        for i in inside:
-            if search.lower() in inside[i].lower():
-                if outputlist:
-                    o.append(i)
-                else:
-                    return i
+        if outputindex:
+            for i in range(len(inside)-1):
+                if search.lower() in inside[inside[i]].lower():
+                    if outputlist:
+                        o.append(i)
+                    else:
+                        return i
+        else:
+            for i in inside:
+                if search.lower() in inside[i].lower():
+                    if outputlist:
+                        o.append(i)
+                    else:
+                        return i
     if outputlist and len(o) > 0:
         return o
     elif errorOut: # If nothing is found and we want it to, it will throw an error
@@ -52,7 +68,7 @@ def unpack(string, file): # Bool on the [1] means if it was searched for (Unexac
 
 def fetch(key,file): # Bool on the [1] means if it was searched for (Unexact match)
     try: # Tries to match exactly with lowercase
-        return file[key].lower(), False
+        return file[key.lower()], False
     except: # Searches every key in the file
         strings=[]
         for i in find(key, file, True):
@@ -62,6 +78,27 @@ def fetch(key,file): # Bool on the [1] means if it was searched for (Unexact mat
         raise Exception("Did not find the key")
                     
 def google(input, target, source): # Renamed string to input to avoid confusion
+
+    '''Returns a tuple: Complete message string, (a number, which determines what type of info is being returned), info (string, list or smth)
+    type numbers:
+    - 0 = an error message
+    - 1 = String: an exact match
+    - 2 = List: unexact match
+    error numbers:
+    - 0.1 = source not in game
+    - 0.2 = source file not found
+    - 0.3 = target not in game
+    - 0.4 = target file not found
+    - 1 = unexpected at ktk
+    - 2 = unexpected at kts
+    - 3 = unexpected at stk
+    - 4 = unexpected at sts
+    - 1.1 = ktk not found
+    - 2.1 = kts not found
+    - 3.1 = stk not found
+    - 4.1 = sts not found
+    '''
+
     # File finding section-----------V
     try: # This is a language file which we will use for keyfinding when source is key
         en_us = json.load(open(langpath + "en_us.json"))
@@ -70,22 +107,34 @@ def google(input, target, source): # Renamed string to input to avoid confusion
         
     if source!="key":
         try: # Find the target language files
-            sourcesrch = find(source, langcodes) # If the code is full, this function will return the same thing, so no need to check
+            try:
+                sourcesrch = langcodes[find(source,langcodesapp,outputindex=True)] # Finds the approved langcode
+            except:
+                try:
+                    sourcesrch = find(source, langcodes) # Finds the internal langcode
+                except:
+                    sourcesrch = langcodes[find(source,langnames,outputindex=True)] # Finds the approved langname
         except:
-            return "This source language is not in the game."
+            return "This source language is not in the game.", 0, 0.1
         try:
             sourcefile = json.load(open(langpath + sourcesrch+".json"))
         except:
-            return "This source language's file does not exist!"
+            return "This source language's file does not exist!", 0, 0.2
     if target!="key":
         try: # Find the source language files
-            targetsrch = find(target, langcodes)
+            try:
+                targetsrch = langcodes[find(target,langcodesapp,outputindex=True)] # Finds the approved langcode
+            except:
+                try:
+                    targetsrch = find(target, langcodes) # Finds the internal langcode
+                except:
+                    targetsrch = langcodes[find(target,langnames,outputindex=True)] # Finds the approved langname
         except:
-            return "This target language is not in the game."
+            return "This target language is not in the game.", 0, 0.3
         try:
             targetfile = json.load(open(langpath + targetsrch + ".json"))
         except:
-            return "This target language's file does not exist!"
+            return "This target language's file does not exist!", 0, 0.4
     # File finding section-----------^
 
     if source == "key":
@@ -93,73 +142,66 @@ def google(input, target, source): # Renamed string to input to avoid confusion
             try:
                 return ktk(input, en_us)
             except:
-                return "An unexpected error has occured at ktk."
+                return "An unexpected error has occured at ktk.", 0, 1
         else:
             try:
                 return kts(input, targetfile)
             except:
-                return "An unexpected error has occured at kts."
+                return "An unexpected error has occured at kts.", 0, 2
     else:
         if target=="key":
             try:
                 return stk(input, sourcefile)
             except:
-                return "An unexpected error has occured at stk."
+                return "An unexpected error has occured at stk.", 0, 3
         else:
             try:
                 return sts(input, sourcefile, targetfile)
             except:
-                return "An unexpected error has occured at sts."
+                return "An unexpected error has occured at sts.", 0, 4
+
 
 def ktk(key, file): # Key to key
-    if debug:
-        print("ktk")
     keys = []
     try:
         for i in find(key, file, True):
             keys.append(i)
     except:
-        return "No keys found. I guess you are stuck outside now ¯\\_(ツ)_/¯"
-    return nearMatch(keys)
+        return "No keys found. I guess you are stuck outside now ¯\\_(ツ)_/¯", 0, 1.1
+    return nearMatch(keys), 2, keys
 
 def kts(key, targetfile): # Key to string
-    if debug:
-        print("kts")
     try:
         strings = fetch(key, targetfile)
     except:
-        return "Did not find the key."
+        return "Did not find the key.", 0, 2.1
     if strings[1]:
-        return nearMatch(strings[0])
+        return nearMatch(strings[0]), 2, strings[0]
     else:
-        return strings[0]
+        return strings[0], 1, strings[0]
 
 def stk(string, sourcefile): # String to key
-    if debug:
-        print("stk")
     try:
         keys=unpack(string, sourcefile)
     except:
-        return "Did not find the string."
+        return "Did not find the string.", 0, 3.1
     if keys[1]:
-        return nearMatch(keys[0])
+        return nearMatch(keys[0]), 2, keys[0]
     else:
-        return keys[0]
+        return keys[0], 1, keys[0]
 
 def sts(string, sourcefile, targetfile): # String to string
-    if debug:
-        print("sts")
     try:
         keys=unpack(string, sourcefile)
     except:
-        return "Did not find the string."
+        return "Did not find the string.", 0, 4.1
     if keys[1]:
         strings=[]
         for i in keys[0]:
-            strings.append(targetfile[i])
-        return nearMatch(strings)
+            strings.append(targetfile[i]) # Intentionally not using fetch, it could return a list in some cases and we don't want that... also, we know that those keys are exact
+        return nearMatch(strings), 2, strings
     else:
-        return fetch(keys[0],targetfile)[0]
+        return targetfile[keys[0]], 1, targetfile[keys[0]]
 
 def nearMatch(of):
     if len(of) == 1:
@@ -172,10 +214,15 @@ for a, b, c in os.walk(langpath): # Gives a list of language codes, so i can sea
     filenames = c
     break
 langcodes = []
+langcodesapp = []
+langnames = []
 for i in filenames:
     langcodes.append(i.split(".")[0])
 for i in filenames:
-    langcodes.append(fetch("language.name", json.load(open(langpath+i)))[0])
+    langnames.append(fetch("language.name", json.load(open(langpath+i)))[0])
+for i in filenames:
+    langcodesapp.append(fetch("language.code", json.load(open(langpath+i)))[0])
+
 # Language names get found and we can fill them in with find() but it isn't the file name so it outputs "file not found" for now... pls fix
     
 @slash.slash(name="translate",
@@ -204,7 +251,7 @@ for i in filenames:
              )
 async def translate(ctx, string, target, source = "en_us"):
     try:
-        await ctx.send(google(string, target, source))
+        await ctx.send(google(string, target, source)[0])
     except:
         print("Error has occured, trying to fallback", string,target ,source)
         try:
