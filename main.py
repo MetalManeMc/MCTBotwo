4386aed18f86684077c02bc7d7361d7e36bb2127
Nan Rosenberg <88463477+Nanrech@users.noreply.github.com>
2021-12-02 16:56:55 +0100

    Remove create_choice again

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import os
 import discord
 from discord_slash import SlashCommand
-from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.utils.manage_commands import create_option
 
 client = discord.Client(intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True)
