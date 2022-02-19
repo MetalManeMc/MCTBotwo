65a5a5d040ce13c39d0816cf9165e393c4838444
MetalManiacMc <93709189+MetalManiacMc@users.noreply.github.com>
2022-02-19 14:48:01 +0100

    Worked on .json converting
    I did 95% of it but I need help BUT I need help

--- filefinder.bedrock.py
+++ filefinder.bedrock.py
@@ -1,6 +1,7 @@
 # This filters the lang files from the default rp
 
 import os
+import json
 
 def rename(initname):
     name=initname.replace('lang', "")
@@ -17,5 +18,52 @@ for dirpath, dirnames, filenames in os.walk("texts"):
 
 for dirpath, dirnames, filenames in os.walk("lang/bedrock"):
     for file in filenames:
-        print(dirpath, file)
-        os.rename(f"{dirpath}/{file}", f"{dirpath}/{rename(file)}") # ... renames the file
+        if ".json" not in file:
+            os.rename(f"{dirpath}/{file}", f"{dirpath}/{rename(file)}") # ... renames the file
+            print(f"{file} renamed into .json")
+
+for dirpath, dirnames, filenames in os.walk("lang/bedrock"):
+    for file in filenames:
+        try:
+            f=open(os.path.join(dirpath, file), 'r+', encoding='utf-8').read()
+            f=f.split("\n")
+            for x in f:
+                old=x
+                #print(x)
+                try:
+                    removefrom = lambda s, ss: s[:s.index(ss) + len(ss)]
+                    x=removefrom(x, "#")
+                    x=x.replace("#", "")
+                    #print(x)
+                except ValueError: pass
+                if "=" in x:
+                    y=x.split("=")
+                    try: 
+                        a=y[2]
+                        together=[y[1], y[2]]
+                        y[1]="=".join(together)
+                        del y[2]
+                    except IndexError:pass
+                    for z in y:
+                        oldz=z
+                        z=z.replace("\t", "")
+                        z=z.replace('"', "'")
+                        z=z.replace('\\', "\\"+"\\")
+                        y[y.index(oldz)]='"'+z+'"'
+                    f[f.index(old)]=":".join(y)
+                    x=":".join(y)
+                    #print(x)
+                else:pass
+            delete=[]
+            for i in f:
+                if "#" in i:
+                    delete.append(f.index(i))
+            for k in delete:
+                i=delete.index(k)
+                f.remove(f[delete[-(i+1)]])
+            f = list(filter(("").__ne__, f))
+            f="{"+",\n".join(f)+"}"
+            open(os.path.join(dirpath, "bin.json"), "r+", encoding='utf-8').write(f)
+            #print(f)
+        except UnicodeDecodeError:pass
+print("Done.")
