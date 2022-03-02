3bdaa33b5432b8712061f4571b5aaef303169582
MetalManiacMc <93709189+MetalManiacMc@users.noreply.github.com>
2022-03-02 21:15:36 +0100

    Sync

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ This path is absolute and independent of the OS in which it may be running.
 DATA_DIR should *not* be altered at any point.
 """
 
-bot = di.Client(token=TOKEN, log_level=31)
+bot = di.Client(token=TOKEN)
 hook = '<:bighook:937813704316158072>'
 
 @bot.event
