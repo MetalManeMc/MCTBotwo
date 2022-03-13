bf23eff2d6ac966c1d5ae79b802961f2890c7403
MetalManiacMc <93709189+MetalManiacMc@users.noreply.github.com>
2022-03-13 14:37:27 +0100

    Detection test 1
    Doesn't reconnect yet

---
+++ down_checker.py
@@ -0,0 +1,22 @@
+import interactions
+import asyncio
+import time
+
+class DownChecker(interactions.Extension):
+    def __init__(self, bot):
+        self.bot: interactions.Client = bot
+        asyncio.get_event_loop().create_task(self.run())
+
+    async def run(self):
+            while True:
+                await self.check_status()
+                await asyncio.sleep(5*60)
+
+    async def check_status(self):
+        print(f"Checking bot status... at {time.strftime('%m-%d-%Y %H:%M:%S')}")
+        if "The client was unable to send a heartbeat, closing the connection." in open("bot.log").read():
+            print("Bot needs to be restarted")
+            open("bot.log", "w").write("")
+
+def setup(bot):
+    DownChecker(bot)
